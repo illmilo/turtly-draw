9a9c4296ceb8a1f03e28a877a375de043e12cf52
illmilo <illfqm@gmail.com>
2025-04-25 12:52:10 +0300

    1. Added image saving possibility. 2. README updated.

--- main.py
+++ main.py
@@ -1,4 +1,8 @@
 from turtle import *
+from PIL import Image
+import os
+
+WIDTH = HEIGHT = 600
 
 def start_new_line(x, y):
     up()
@@ -8,15 +12,28 @@ def start_new_line(x, y):
 def draw(x, y): 
     goto(x, y)
 
+def save_image():
+    hideturtle()
+    canvas = screen.getcanvas()
+    canvas.postscript(file="main.eps", width=WIDTH, height=HEIGHT)
+    ps_image = Image.open("main.eps")
+    ps_image.save("main.png")
+    os.remove("main.eps")
+    showturtle()
+
 screen = Screen()
-screen.setup(600, 600)
+screen.setup(WIDTH, HEIGHT)
 
 left(90)
 speed(0)
-screen.onclick(start_new_line)
 
+screen.onclick(start_new_line)
 ondrag(None)  # unload the memory usage to avoid lags
 ondrag(draw)
+
+# clicking the "s" key will save the image
+screen.onkey(save_image, "s")
+screen.listen()
 
 # keep the program running until we close it
 screen.mainloop()
--- readme.md
+++ readme.md
@@ -1,8 +1,20 @@
+<img src="https://cdn-icons-png.freepik.com/256/1650/1650608.png?semt=ais_hybrid" width=20% align=right>
+
 # turtly-draw
 ## Introduction
-A simple drawing tool made with Python built-in `turtle` module.
+A simple drawing tool made with Python built-in `turtle` module. The greatest way to show the possibillities of Python built-in graphic tools.
+
+### Capabilities
+- Press `s` to save `.png` image into directory of the project
+
+<img src="main.png" width=50% alt="Showcase">
+
 ## Requirements
 - **Tk** interface package (**Tkinter**)
+- **PIL** image manager (**Pillow**)
+```bash
+pip install -r requirements.txt
+```
 ## Installation
 ```bash
 git clone https://illmilo/turtly-draw.git
